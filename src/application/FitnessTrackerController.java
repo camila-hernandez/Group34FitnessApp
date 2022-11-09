d0877985140b27f0289f09e9f2c8f7cbeb1c3e2e
Camila Hernandez <camila.hernandez@ucalgary.ca>
2022-11-09 16:35:58 +0000

    Created FitnessTrackerController and added methods for each button

---
+++ src/application/FitnessTrackerController.java
@@ -0,0 +1,50 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FitnessTrackerController {
+
+    @FXML
+    void openUserProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackSteps(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackSleep(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackNutrition(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackWeight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackActivities(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showRecommendations(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setGoals(ActionEvent event) {
+
+    }
+
+}
+
+
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module Group34FitnessApp {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
