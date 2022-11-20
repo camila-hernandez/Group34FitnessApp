1e919394fa07a0b0b30ca9c267fdff8f2e548174
Mariam <Mariam@10.0.0.127>
2022-11-20 15:48:32 -0700

    added button WaterIntake which opens a new window, and a textfield which
    takes how much L of water you drank.

--- src/application/FitnessTrackerController.java
+++ src/application/FitnessTrackerController.java
@@ -1,17 +1,28 @@
 package application;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FitnessTrackerController {
 	Stage applicationStage;
+	
+	@FXML
+	private TextField amountOfWater;
+	
+	@FXML 
+	private Label waterProgress;
 	
     @FXML
     void openUserProfile(ActionEvent event) {
@@ -41,6 +52,32 @@ public class FitnessTrackerController {
     @FXML
     void trackActivities(ActionEvent event) {
 
+    }
+    
+    @FXML
+    void calculateAmountWater(ActionEvent progressWaterEvent) {
+    	Double liters = Double.parseDouble(amountOfWater.getText());
+    	Double waterGoal = 7.0;
+    	waterProgress.setText("You are " + (waterGoal - liters) + " L away from your goal.");
+    	
+    	
+    }
+    
+    @FXML
+    void trackWaterIntake(ActionEvent setWaterIntakeEvent) {
+
+    	try {
+			FXMLLoader loader = new FXMLLoader();
+    	    BorderPane root = loader.load(new FileInputStream("src/application/UserWaterIntake.fxml"));
+    	    FitnessTrackerController controller = (FitnessTrackerController)loader.getController();
+    	    controller.applicationStage = applicationStage;
+    	    Scene scene = new Scene(root);
+    	    applicationStage.setScene(scene);
+    	    applicationStage.show();
+    	  } catch (IOException e) {
+    		  
+    	  }
+    	
     }
 
     @FXML
