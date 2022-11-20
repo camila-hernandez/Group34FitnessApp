c7d8fb5377a22d23686126f50691af6e4c6a3c11
Camila Hernandez <camila.hernandez@ucalgary.ca>
2022-11-20 16:54:33 -0700

    Added actions to returnToMainPage action method

--- src/application/UserGoalsDisplay.java
+++ src/application/UserGoalsDisplay.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.io.FileInputStream;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -79,7 +84,18 @@ public class UserGoalsDisplay {
     
     @FXML
     void returnToMainPage(ActionEvent event) {
-
+    	 try {
+  		   FXMLLoader loader = new FXMLLoader();
+  		   BorderPane root = loader.load(new FileInputStream("src/application/FitnessTrackerView.fxml"));
+  		   FitnessTrackerController controller = (FitnessTrackerController)loader.getController();
+  		   controller.applicationStage = applicationStage;
+  		   
+  		   Scene scene = new Scene(root);
+  		   applicationStage.setScene(scene);
+  		   applicationStage.show();
+  	   } catch(Exception e) {
+  		   e.printStackTrace();
+  	   }
     }
 
     @FXML
