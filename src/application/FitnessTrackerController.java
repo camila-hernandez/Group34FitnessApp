ada649e1efbc54ca4c2a426f82a128ef6cc57b4a
Camila Hernandez <camila.hernandez@ucalgary.ca>
2022-11-20 12:14:51 -0700

    Created a new scene to update user health/fitness goals

--- src/application/FitnessTrackerController.java
+++ src/application/FitnessTrackerController.java
@@ -11,6 +11,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -81,6 +82,7 @@ public class FitnessTrackerController {
    @FXML
    void showUserFitnessGoals(ActionEvent event) {
 	   Scene mainScene = applicationStage.getScene();
+	   Scene updateUserFitnessGoalsScene = applicationStage.getScene();
 	   
 	   // Create new scene that displays the user's health and fitness goals
 	   VBox fitnessGoalsDisplayContainer = new VBox();
@@ -200,7 +202,7 @@ public class FitnessTrackerController {
 	   updateGoalsButton.setTextFill(Color.WHITE);
 	   updateGoalsButton.setFont(labelFont);
 	   updateGoalsButton.setStyle("-fx-background-color: LIGHTBLUE");
-	  // updateGoalsButton.setOnAction(updateGoalsEvent -> applicationStage.setScene());
+	   updateGoalsButton.setOnAction(updateGoalsEvent -> updateUserFitnessGoals(updateUserFitnessGoalsScene));
 	   
 	   buttonsContainer.getChildren().addAll(homeButton, updateGoalsButton);
 	   buttonsContainer.setAlignment(Pos.CENTER);
@@ -211,6 +213,134 @@ public class FitnessTrackerController {
 		
 	   Scene displayUserFitnessGoalsScene = new Scene(fitnessGoalsDisplayContainer, 609, 856);
 	   applicationStage.setScene(displayUserFitnessGoalsScene);
+   }
+   
+   void updateUserFitnessGoals(Scene updateUserFitnessGoalsScene) {
+	   Scene displayuserFitnessGoalsScene = applicationStage.getScene(); 
+	   
+	   // Create new scene that displays the user's health and fitness goals
+	   VBox updateFitnessGoalsContainer = new VBox();
+	   updateFitnessGoalsContainer.setStyle("-fx-background-color: white");
+	   
+	   // Create header title
+	   StackPane updateStack = new StackPane();
+	   
+	   Rectangle updateHeaderRectangle = new Rectangle(87, 58, 435, 93);
+	   updateHeaderRectangle.setFill(Color.LIGHTBLUE);
+	   updateHeaderRectangle.setArcHeight(20);
+	   updateHeaderRectangle.setArcWidth(20);
+	   
+	   Label updateFitnessGoalsLabel = new Label("UPDATE HEALTH AND FITNESS GOALS");
+	   updateFitnessGoalsLabel.setTextFill(Color.WHITE);
+	   Font font = Font.font("System", FontWeight.BOLD, 22);
+	   updateFitnessGoalsLabel.setFont(font);
+	   
+	   updateStack.getChildren().addAll(updateHeaderRectangle, updateFitnessGoalsLabel);
+	   updateStack.setPadding(new Insets(25,0,25,0));
+	   
+	   // Create steps container
+	   HBox updateStepsGoalsContainer = new HBox();
+	   updateStepsGoalsContainer.setPadding(new Insets(0,50,0,50));
+	   Label updateStepsGoalsLabel = new Label("STEPS GOALS:");
+	   Font labelFont = Font.font("System", FontWeight.BOLD, 24);
+	   updateStepsGoalsLabel.setFont(labelFont);
+	   updateStepsGoalsLabel.setTextFill(Color.LIGHTBLUE);
+	   updateStepsGoalsLabel.setPadding(new Insets(25, 0, 25, 0));
+	   TextField stepsGoalsTextfield = new TextField();
+	   stepsGoalsTextfield.setMinWidth(259);
+	   stepsGoalsTextfield.setMinHeight(51);
+	   updateStepsGoalsContainer.getChildren().addAll(updateStepsGoalsLabel, stepsGoalsTextfield);
+	   updateStepsGoalsContainer.setAlignment(Pos.CENTER);
+	   
+	   // Create sleep container
+	   HBox updateSleepGoalsContainer = new HBox();
+	   Label updateSleepGoalsLabel = new Label("SLEEP GOALS:");
+	   updateSleepGoalsLabel.setFont(labelFont);
+	   updateSleepGoalsLabel.setTextFill(Color.LIGHTBLUE);
+	   updateSleepGoalsLabel.setPadding(new Insets(25, 5, 25, 0));
+	   TextField updateSleepGoalsTextfield = new TextField();
+	   updateSleepGoalsTextfield.setMinWidth(254);
+	   updateSleepGoalsTextfield.setMinHeight(51);
+	   updateSleepGoalsContainer.getChildren().addAll(updateSleepGoalsLabel, updateSleepGoalsTextfield);
+	   updateSleepGoalsContainer.setAlignment(Pos.CENTER);
+	   
+	   // Create water in-take container
+	   HBox updateWaterIntakeGoalsContainer = new HBox();
+	   Label updateWaterIntakeGoalsLabel = new Label("WATER INTAKE GOALS:");
+	   updateWaterIntakeGoalsLabel.setTextFill(Color.LIGHTBLUE);
+	   updateWaterIntakeGoalsLabel.setPadding(new Insets(25, 0, 25, 52));
+	   updateWaterIntakeGoalsLabel.setFont(labelFont);
+	   TextField updateWaterIntakeGoalsTextfield = new TextField();
+	   updateWaterIntakeGoalsTextfield.setMinWidth(152);
+	   updateWaterIntakeGoalsTextfield.setMinHeight(51);
+	   updateWaterIntakeGoalsContainer.getChildren().addAll(updateWaterIntakeGoalsLabel, updateWaterIntakeGoalsTextfield);
+	   updateWaterIntakeGoalsContainer.setAlignment(Pos.CENTER);
+	   
+	   // Create nutrition container
+	   Label updateNutritionGoalsLabel = new Label("NUTRITION GOALS:");
+	   updateNutritionGoalsLabel.setTextFill(Color.LIGHTBLUE);
+	   updateNutritionGoalsLabel.setPadding(new Insets(25, 0, 10, 90));
+	   updateNutritionGoalsLabel.setMinSize(224, 35);
+	   updateNutritionGoalsLabel.setFont(labelFont);
+		
+	   HBox updateNutritionGoalsContainer = new HBox();
+	   Label updateWeightGoalsLabel = new Label("What is your weight goal (in kg)?");
+	   updateWeightGoalsLabel.setPadding(new Insets(0, 0, 0, 50));
+	   Font textFont = Font.font("System", 18);
+	   updateWeightGoalsLabel.setFont(textFont);
+	   TextField weightGoalsTextfield = new TextField();
+	   weightGoalsTextfield.setMinSize(152, 51);
+	   updateNutritionGoalsContainer.getChildren().addAll(updateWeightGoalsLabel, weightGoalsTextfield);
+	   updateNutritionGoalsContainer.setAlignment(Pos.CENTER);
+	   
+	   // Create exercise container
+	   Label updateExerciseGoalsLabel = new Label("EXERCISE GOALS:");
+	   updateExerciseGoalsLabel.setPadding(new Insets(25, 0, 15, 90));
+	   updateExerciseGoalsLabel.setTextFill(Color.LIGHTBLUE);
+	   updateExerciseGoalsLabel.setMinSize(224, 35);
+	   updateExerciseGoalsLabel.setFont(labelFont);
+		
+	   VBox updateExerciseGoalsContainer = new VBox();
+	   Label updateExerciseDurationGoalsLabel = new Label("How long would you like to exercise (in minutes)?");
+	   updateExerciseDurationGoalsLabel.setPadding(new Insets(0, 32, 25, 0));
+	   updateExerciseDurationGoalsLabel.setFont(textFont);
+	   TextField exerciseDurationGoalsTextfield = new TextField();
+	   exerciseDurationGoalsTextfield.setMinSize(298, 51);
+	   exerciseDurationGoalsTextfield.setAlignment(Pos.CENTER);
+	   updateExerciseGoalsContainer.getChildren().addAll(updateExerciseDurationGoalsLabel, exerciseDurationGoalsTextfield);
+	   updateExerciseGoalsContainer.setAlignment(Pos.CENTER);
+	   
+	   // Create calories burned container
+	   VBox updateCaloriesGoalsContainer = new VBox();
+	   Label updateCaloriesBurnedGoalsLabel = new Label("How many calories would you like to burn in a day?");
+	   updateCaloriesBurnedGoalsLabel.setPadding(new Insets(0, 15, 25, 0));
+	   updateCaloriesBurnedGoalsLabel.setFont(textFont);
+	   TextField caloriesBurnedGoalsTextfield = new TextField();
+	   caloriesBurnedGoalsTextfield.setMinSize(298, 51);
+	   caloriesBurnedGoalsTextfield.setAlignment(Pos.CENTER);
+	   updateCaloriesGoalsContainer.getChildren().addAll(updateCaloriesBurnedGoalsLabel, caloriesBurnedGoalsTextfield);
+	   updateCaloriesGoalsContainer.setAlignment(Pos.CENTER);
+	   
+	   // Create buttons container
+	   HBox updateButtonContainer = new HBox();
+	   
+	   Button saveChangesGoalsButton = new Button("SAVE CHANGES");
+	   saveChangesGoalsButton.setMinSize(177, 44);
+	   saveChangesGoalsButton.setTextFill(Color.WHITE);
+	   saveChangesGoalsButton.setFont(labelFont);
+	   saveChangesGoalsButton.setStyle("-fx-background-color: LIGHTBLUE");
+	   saveChangesGoalsButton.setOnAction(saveChangesGoalsEvent -> applicationStage.setScene(displayuserFitnessGoalsScene));
+	   
+	   updateButtonContainer.getChildren().add(saveChangesGoalsButton);
+	   updateButtonContainer.setAlignment(Pos.CENTER);
+	   updateButtonContainer.setSpacing(20);
+		
+	   updateFitnessGoalsContainer.getChildren().addAll(updateStack, updateStepsGoalsContainer, updateSleepGoalsContainer, updateWaterIntakeGoalsContainer,
+				updateNutritionGoalsLabel, updateNutritionGoalsContainer, updateExerciseGoalsLabel, updateExerciseGoalsContainer, updateCaloriesGoalsContainer, updateButtonContainer);
+		
+	   Scene updatedUserFitnessGoalsScene = new Scene(updateFitnessGoalsContainer, 609, 856);
+	   applicationStage.setScene(updatedUserFitnessGoalsScene);
+	   
    }
 }
     	
