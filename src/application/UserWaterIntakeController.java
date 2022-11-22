1d4c0aa72dc58f0500a242aedec0e8d80b930b29
Mariam <Mariam.masri@ucalgary.ca>
2022-11-21 18:58:03 -0700

    created separate controller for waterIntake which includes a returnToMain button.

--- src/application/UserWaterIntakeController.java
+++ src/application/UserWaterIntakeController.java
@@ -1,9 +1,15 @@
 package application;
 
+import java.io.FileInputStream;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class UserWaterIntakeController {
@@ -21,7 +27,22 @@ public class UserWaterIntakeController {
 		Double waterGoal = 7.0;
 		waterProgress.setText("You are " + (waterGoal - liters) + " L away from your goal.");
 
-
+	}
+	
+	@FXML
+    void returnToMain(ActionEvent event) {
+    	 try {
+  		   FXMLLoader loader = new FXMLLoader();
+  		   BorderPane root = loader.load(new FileInputStream("src/application/FitnessTrackerView.fxml"));
+  		   FitnessTrackerController controller = (FitnessTrackerController)loader.getController();
+  		   controller.applicationStage = applicationStage;
+  		   
+  		   Scene scene = new Scene(root);
+  		   applicationStage.setScene(scene);
+  		   applicationStage.show();
+  	   } catch(Exception e) {
+  		   e.printStackTrace();
+  	   }
 	}
 
 }
