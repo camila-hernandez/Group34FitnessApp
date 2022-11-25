9d912de57209be6f78e5c4bf3b3554647dee0ea5
EnesGisi <enes.gisi@ucalgary.ca>
2022-11-25 10:45:17 -0700

    Switch to steps fxml

--- src/application/FitnessTrackerController.java
+++ src/application/FitnessTrackerController.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.io.FileInputStream;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -25,7 +29,18 @@ public class FitnessTrackerController {
 
     @FXML
     void trackSleep(ActionEvent event) {
-
+    	 try {
+  		   FXMLLoader loader = new FXMLLoader();
+  		   AnchorPane root = loader.load(new FileInputStream("src/application/StepsView.fxml"));
+  		   UserGoalsDisplay controller = (UserGoalsDisplay)loader.getController();
+  		   controller.applicationStage = applicationStage;
+  		   
+  		   Scene scene = new Scene(root);
+  		   applicationStage.setScene(scene);
+  		   applicationStage.show();
+  	   } catch(Exception e) {
+  		   e.printStackTrace();
+  	   }
     }
 
     @FXML
