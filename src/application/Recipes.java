942e5f6cb3a763db373499ae9acadcd617f17818
Camila Hernandez <camila.hernandez@ucalgary.ca>
2022-11-26 20:48:57 -0700

    Fixed layout of breakfast burrito recipe

--- src/application/Recipes.java
+++ src/application/Recipes.java
@@ -1,11 +1,19 @@
 package application;
 
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Recipes {
@@ -17,49 +25,60 @@ public class Recipes {
     	
     	// Source: https://www.foodnetwork.com/recipes/ellie-krieger/breakfast-burrito-recipe-1953146
     	VBox burritoRecipeContainer = new VBox();
-    	Label burritoRecipe = new Label();
+    	burritoRecipeContainer.setStyle("-fx-background-color: white");
+    	HBox burritoNameContainer = new HBox();
+    	Label burritoRecipeIngredients = new Label();
+  	   	Font font = Font.font("System", FontWeight.BOLD, 18);
     	Label burritoNameLabel = new Label("Breakfast Burrito");
-    	burritoRecipe.setText(
-    			"Ingredients\r\n"
-    			+ "2 teaspoons canola oil\r\n"
+    	burritoNameContainer.getChildren().add(burritoNameLabel);
+    	burritoNameContainer.setAlignment(Pos.CENTER);
+    	burritoNameLabel.setFont(font);
+    	Label burritoIngredients = new Label("Ingredients:");
+    	Font ingredientsFont = Font.font("System", FontWeight.BOLD, 16);
+    	burritoIngredients.setFont(ingredientsFont);
+    	burritoRecipeIngredients.setText(
+    			"2 teaspoons canola oil"
     			+ "\r\n"
-    			+ "1/2 small red onion, diced (1 cup)\r\n"
+    			+ "1/2 small red onion, diced (1 cup)"
     			+ "\r\n"
-    			+ "1 red bell pepper, seeded and diced\r\n"
+    			+ "1 red bell pepper, seeded and diced"
     			+ "\r\n"
-    			+ "1 cup drained, rinsed canned black beans, preferably low-sodium\r\n"
+    			+ "1 cup drained, rinsed canned black beans, preferably low-sodium"
     			+ "\r\n"
-    			+ "1/4 teaspoon chili flakes\r\n"
+    			+ "1/4 teaspoon chili flakes"
     			+ "\r\n"
-    			+ "Salt and freshly ground black pepper\r\n"
+    			+ "Salt and freshly ground black pepper"
     			+ "\r\n"
-    			+ "4 eggs and 4 egg whites\r\n"
+    			+ "4 eggs and 4 egg whites"
     			+ "\r\n"
-    			+ "1/3 cup (about 1 1/2 ounce) shredded pepper Jack cheese\r\n"
+    			+ "1/3 cup (about 1 1/2 ounce) shredded pepper Jack cheese"
     			+ "\r\n"
-    			+ "Nonstick cooking spray\r\n"
+    			+ "Nonstick cooking spray"
     			+ "\r\n"
-    			+ "4 (10 inch) whole wheat tortillas (burrito size)\r\n"
+    			+ "4 (10 inch) whole wheat tortillas (burrito size)"
     			+ "\r\n"
-    			+ "1/4 cup reduced fat-free sour cream or 2 percent plain Greek yogurt\r\n"
+    			+ "1/4 cup reduced fat-free sour cream or 2 percent plain Greek yogurt"
     			+ "\r\n"
-    			+ "1/4 cup salsa\r\n"
+    			+ "1/4 cup salsa"
     			+ "\r\n"
-    			+ "1 large tomato, (4 ounces) seeded and diced\r\n"
+    			+ "1 large tomato, (4 ounces) seeded and diced"
     			+ "\r\n"
-    			+ "1 small avocado (4 ounces), cubed\r\n"
+    			+ "1 small avocado (4 ounces), cubed"
     			+ "\r\n"
     			+ "Hot sauce"
-    			+ "\r\n"
-    			+ "Recipe"
-    			+ "\r\n"
-    			+ "Heat the canola oil in a large nonstick skillet over a medium-high heat."
+    			+ "\r\n");
+    	Label burritoDirectionsLabel = new Label("Directions:");
+    	Font directionsFont = Font.font("System", FontWeight.BOLD, 16);
+    	burritoDirectionsLabel.setFont(directionsFont);
+    	Label burritoRecipe = new Label();
+    	burritoRecipe.setText(
+    			"Heat the canola oil in a large nonstick skillet over a medium-high heat."
     			+ "\r\n"
     			+ "Cook the onions and peppers until onions are softened and peppers are slightly charred, about 8 minutes."
     			+ "\r\n"
     			+ "Add black beans and red pepper flakes and cook until warmed through, another 3 minutes."
     			+ "\r\n"
-    			+ "Season with salt and pepper and transfer to a dish."
+    			+ "Season with salt and pepper and transfer to a dish. Whisk together the eggs and egg whites then stir in the cheese."
     			+ "\r\n"
     			+ "Whisk together the eggs and egg whites then stir in the cheese."
     			+ "\r\n"
@@ -67,14 +86,19 @@ public class Recipes {
     			+ "\r\n"
     			+ "Reduce heat to low and add eggs, scrambling until cooked through, about 3 minutes."
     			+ "\r\n"
-    			+ "Spread each tortilla with 1 tablespoon each sour cream (or yogurt) and salsa,"
+    			+ "Spread each tortilla with 1 tablespoon each sour cream (or yogurt) and salsa, then layer with 1/4 of the black bean"
     			+ "\r\n"
-    			+ "then layer with 1/4 of the black bean mixture, 1/4 of the scrambled eggs, some diced tomato"
+    			+ "mixture, 1/4 of the scrambled eggs, some diced tomato and 1/4 of the avocado."
     			+ "\r\n"
-    			+ "and 1/4 of the avocado. Season, to taste, with hot sauce. Roll up burrito-style and serve.");
+    			+ "Season, to taste, with hot sauce. Roll up burrito-style and serve.");
+    	HBox doneButtonContainer = new HBox();
     	Button doneButton = new Button("Done");
+    	doneButtonContainer.getChildren().add(doneButton);
+    	doneButtonContainer.setAlignment(Pos.CENTER);
+    	doneButtonContainer.setPadding(new Insets(20,0,0,0));
     	doneButton.setOnAction(doneEvent -> applicationStage.setScene(mainScene));
-    	burritoRecipeContainer.getChildren().addAll(burritoNameLabel, burritoRecipe, doneButton);
+    	burritoRecipeContainer.getChildren().addAll(burritoNameContainer, burritoIngredients, burritoRecipeIngredients, 
+    			burritoDirectionsLabel, burritoRecipe, doneButtonContainer);
     	
     	Scene breakfastBurritoScene = new Scene(burritoRecipeContainer, 609, 856);
    	   	applicationStage.setScene(breakfastBurritoScene);
@@ -203,7 +227,6 @@ public class Recipes {
 
     @FXML
     void showMisoSoupRecipe(ActionEvent event) {
-    	
     }
 
     @FXML
