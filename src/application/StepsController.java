1173bd5c524dce8438b8416d747bc9c3e80dc04c
EnesGisi <enes.gisi@ucalgary.ca>
2022-11-30 15:39:19 -0700

    StepsController removed some imports

--- src/application/StepsController.java
+++ src/application/StepsController.java
@@ -5,20 +5,10 @@ import java.io.FileInputStream;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class StepsController {
