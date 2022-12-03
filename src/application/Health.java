623fae634b77f6ae7ef69e4fe9ef199f09d002c8
masrim1 <113072377+masrim1@users.noreply.github.com>
2022-12-03 14:35:28 -0700

    added carbs, fat, fiber variables and setter, getters. also updated bmi
    to use weight, and height from userProfile.

--- src/application/Health.java
+++ src/application/Health.java
@@ -12,6 +12,9 @@ public class Health extends Goals {
 	private String waterProgressTotalLabel;
 	private double sleepDuration;
 	private String sleepProgressLabel;
+	private double carbs;
+	private double fat;
+	private double fiber;
 	
 	double value;
 	
@@ -122,6 +125,8 @@ public class Health extends Goals {
 	}
 	
 	public double calculateBMI() {
+		weight = getWeight();
+		height = getHeight()/100;
 		return weight / (height * height);
 	}
 	
@@ -153,17 +158,40 @@ public class Health extends Goals {
 		this.sleepProgressLabel = sleepProgressLabel;
 	}
 	
-	//double calculateCarbs() {
-		//return (amount in serving size (in g) / 300) * 100;
-	//}
+	double calculateCarbs() {
+		
+		return (getCarbs() / 30) * 100;
+	}
 	
-	//double calculateFat() {
-		//return (amount in serving size (in g) / 65) * 100;
-	//}
+	double calculateFat() {
+		return (getFat() / 65) * 100;
+	}
 	
-	//double calculateFiber() {
-		//return (amount in serving size (in g) / 25) * 100;
-	//}
-	
+	double calculateFiber() {
+		return (getFiber() / 25) * 100;
+	}
 
+	public double getCarbs() {
+		return carbs;
+	}
+
+	public void setCarbs(double carbs) {
+		this.carbs = carbs;
+	}
+
+	public double getFat() {
+		return fat;
+	}
+
+	public void setFat(double fat) {
+		this.fat = fat;
+	}
+
+	public double getFiber() {
+		return fiber;
+	}
+
+	public void setFiber(double fiber) {
+		this.fiber = fiber;
+	}
 }
